ebea15d81712ed866eaa4b16c639705cd5f6b171
Niclas Björner <niclas@fulkod.nu>
2015-05-14 16:47:01 +0200

    Had to update the endpoints
    The API didn't like the trailing slash for me and id/

--- src/main/java/se/cromigon/beepsend/api/Endpoints.java
+++ src/main/java/se/cromigon/beepsend/api/Endpoints.java
@@ -4,6 +4,6 @@ package se.cromigon.beepsend.api;
 public interface Endpoints {
     String BASE_URL = "https://api.beepsend.com/2";
     String CONNECTIONS = "/connections/";
-    String CONNECTIONS_ME = "/connections/me/";
-    String CONNECTIONS_ID = "/connections/%d/";
+    String CONNECTIONS_ME = "/connections/me";
+    String CONNECTIONS_ID = "/connections/%d";
 }
