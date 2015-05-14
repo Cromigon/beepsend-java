bd3b7c01c3fddb6d2dd779517f7063761610f225
Niclas Björner <niclas@fulkod.nu>
2015-05-14 16:26:09 +0200

    Lifted Endpoints to be in a separate file
    This was done due to me feeling that it would be easier to work with the
    endpoints that way.

---
+++ src/main/java/se/cromigon/beepsend/api/Endpoints.java
@@ -0,0 +1,9 @@
+package se.cromigon.beepsend.api;
+
+
+public interface Endpoints {
+    String BASE_URL = "https://api.beepsend.com/2";
+    String CONNECTIONS = "/connections/";
+    String CONNECTIONS_ME = "/connections/me/";
+    String CONNECTIONS_ID = "/connections/%d/";
+}
